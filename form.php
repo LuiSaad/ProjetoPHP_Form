c2bc77959131da60db6dab610027e15ff2eca8ab
Cyntia Lino <100882616+CynGL@users.noreply.github.com>
2023-05-10 20:14:43 -0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> oi </title>
+</head>
+<body>
+<style type="text/css">
+	.btn{
+		width: 10vw;
+		height: 40px;
+		background: #FF9F8A;
+	}
+
+</style>
+
+<h1> Oi, tudo bem?</h1>
+	<button class="btn">
+		botão
+	</button>
+</body>
+</html>
